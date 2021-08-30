a2f230b07ec777e0fd76479ba2313bdbc535c782
Apk <42800741+PremApk@users.noreply.github.com>
2021-08-30 21:01:37 +0530

    Removed Unwanted Function

--- echo_bot.py
+++ echo_bot.py
@@ -19,10 +19,5 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 """)
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
 
 bot.polling()
